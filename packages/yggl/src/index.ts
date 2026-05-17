45ac80a70393c3bb8a3272155448d45c8523b535
Ivan <51127712+VANADAIN@users.noreply.github.com>
2026-05-17 21:31:06 +0700

    chore: export phase 2 modules and add .yggl to gitignore

--- packages/yggl/src/index.ts
+++ packages/yggl/src/index.ts
@@ -1,3 +1,28 @@
 export const version = '0.1.0'
 export type { BinaryResolverDeps, DaemonMode } from './binary.js'
 export { BinaryNotFoundError, resolveBinary } from './binary.js'
+export type { AdminSocketConfig, AuthConfig, YgglConfig } from './config.js'
+export {
+	CONFIG_FILENAME,
+	ConfigError,
+	DEFAULT_CONFIG,
+	DEFAULT_PEERS,
+	loadConfig,
+	validateConfig,
+	writeDefaultConfig,
+} from './config.js'
+export type { DaemonSource, DetectionDeps, DetectionResult } from './daemon.js'
+export {
+	DaemonError,
+	DaemonManager,
+	detectDaemon,
+	initYggstackConf,
+	YGGL_DIR,
+	YGGSTACK_CONF,
+} from './daemon.js'
+export type { MulticastInterface, YggstackConfig } from './yggstack-conf.js'
+export {
+	DEFAULT_MULTICAST_INTERFACE,
+	mergeYggstackConfig,
+	parseYggstackConfig,
+} from './yggstack-conf.js'
